83f4ef8b037b8fe141ac0cc4e8616da7127b86ea
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-11 11:16:08 -0500

    refactor: remove ranking router package reexport

--- app/domains/ranking/__init__.py
+++ app/domains/ranking/__init__.py
@@ -3,11 +3,9 @@
 from app.domains.ranking import service
 from app.domains.ranking import repository
 from app.domains.ranking import schemas
-from app.domains.ranking import router
 
 __all__ = [
     "service",
     "repository",
     "schemas",
-    "router",
 ]
